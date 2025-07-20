75e16edfdbe34db4684513f2f2630865e0492c07
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2025-07-19 23:58:58 -0300

    feat: add laravel telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
 ];
+
+if (class_exists(Laravel\Telescope\TelescopeServiceProvider::class)) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
