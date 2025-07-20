745b9505f9859fdd9119c8d9cc23e9ee3b070937
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2025-07-20 01:09:04 -0300

    fix: arch tests

--- app/Providers/Filament/AdminPanelServiceProvider.php
+++ app/Providers/Filament/AdminPanelServiceProvider.php
@@ -22,7 +22,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-final class AdminPanelProvider extends PanelProvider
+final class AdminPanelServiceProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $providers = [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\AdminPanelServiceProvider::class,
 ];
 
 if (class_exists(Laravel\Telescope\TelescopeServiceProvider::class)) {
