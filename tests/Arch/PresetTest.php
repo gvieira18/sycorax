2e139446cf80972887792296fb4eb7c520fc9162
Gabriel do Carmo Vieira <48625433+gvieira18@users.noreply.github.com>
2025-07-24 17:46:13 -0300

    test: enhance architectural rules

--- tests/Arch/PresetTest.php
+++ tests/Arch/PresetTest.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
-arch()->preset()->php();
-arch()->preset()->laravel();
-arch()->preset()->security();
+arch('application must follow the defined Laravel architectural rules')
+    ->preset()
+    ->laravel();
+
+arch('application must follow the defined PHP architectural rules')
+    ->preset()
+    ->php();
+
+arch('application must follow security best practices')
+    ->preset()
+    ->security();
